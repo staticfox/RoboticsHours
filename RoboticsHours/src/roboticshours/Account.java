12e366283f7aa5f4d17c7d728ce1db83a16bc5d9
Alan Huang <alanhuang122@gmail.com>
2015-02-24 19:18:06 -0600

    Update Javadoc and documentation for Account class

--- RoboticsHours/src/roboticshours/Account.java
+++ RoboticsHours/src/roboticshours/Account.java
@@ -3,18 +3,21 @@ package roboticshours;
 import java.util.ArrayList;
 
 /**
- *
+ * A class to organize hour entries.
+ * This class contains an ID, name, and <code>entryList</code>,
+ * an {@link ArrayList} used to store entries.
+ * 
  * @author Alan
  */
 public class Account {
 
     private static int numberOfAccounts; //UNNECESSARY: TODO REMOVE
     private final int ID; //Primary key?
-    private String name; //TODO FIRST AND LAST NAME
+    private String name; //TODO FIRST AND LAST NAME. Possibly concatenate users' names into one string without spaces.
     private final ArrayList<Entry> entryList;
 
     /**
-     * Creates a new account.
+     * Creates a new account
      * @param n the user's name
      */
     public Account(String n) { //Constructor A: Users should use this constructor.
@@ -25,7 +28,7 @@ public class Account {
     }
 
     /**
-     * Creates a new account and specifies the ID number to be used.
+     * Creates a new account and specifies the ID number to be used
      * @param i custom ID number
      * @param n the user's name
      */
@@ -58,7 +61,7 @@ public class Account {
     }
 
     /**
-     * Adds an <code>Entry</code> object to the internal <code>ArrayList</code> within the <code>Account</code>.
+     * Adds an <code>Entry</code> object to the internal <code>ArrayList</code> within the <code>Account</code>
      * @param e the <code>Entry</code> to be added
      */
     public void addEntry(Entry e) {
@@ -66,23 +69,21 @@ public class Account {
     }
 
     /**
-     *
-     * @param e
+     * @param e the <code>Entry</code> to be removed from the <code>entryList</code>
      */
     public void deleteEntry(Entry e) {
         entryList.remove(e);
     }
 
     /**
-     *
-     * @return
+     * @return  the <code>ArrayList</code> of entries
      */
     public ArrayList<Entry> getEntries() {
         return entryList;
     }
 
     /**
-     *  Clears all the entries in entryList.
+     *  Clears all the entries in <code>entryList</code>.
      */
     public void clearEntries() {
         entryList.clear();
@@ -90,6 +91,7 @@ public class Account {
     
     /**
      *  Increases the current counter for account IDs.
+     *  This will most likely be removed in a new version.
      */
     public static void incrementNumberOfAccounts(){
         numberOfAccounts++;
